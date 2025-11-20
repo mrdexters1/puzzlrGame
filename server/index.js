ac0098608a72232d328e7706e2578efca22d36b4
Vadym Beletskyi <mrdexters1@gmail.com>
2025-11-20 19:24:41 +0100

    feat: port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import cors from "cors";
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(
   cors({
